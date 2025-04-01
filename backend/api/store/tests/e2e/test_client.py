703957c1490866283ecd15a45ef3ee226938d692
vanessapigwin <72374043+vanessapigwin@users.noreply.github.com>
2025-04-02 00:42:01 +0800

    add test for catefgory fetch, remove dotenv

---
+++ backend/api/store/tests/e2e/test_client.py
@@ -0,0 +1,18 @@
+from django.test import Client
+import pytest
+
+pytestmark = pytest.mark.django_db
+
+@pytest.fixture(scope='module')
+def client():
+    return Client()
+    
+
+def test_get_categories(client):
+    response = client.get("/categories/")
+    content = response.json()
+    assert set(['next', 'previous', 'results']).issubset(content.keys())
+
+    expeted_entry = {'category_id': 1, 'category_name': 'accessories'}
+    actual_entry = content.get('results')[0]
+    assert actual_entry == expeted_entry
