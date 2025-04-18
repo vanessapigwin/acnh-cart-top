4c9bc57e370102a1e4694cdce8ba9befcd33cddb
vanessapigwin <72374043+vanessapigwin@users.noreply.github.com>
2025-04-19 01:00:43 +0800

    add categ negative tests

--- backend/api/store/tests/e2e/test_client.py
+++ backend/api/store/tests/e2e/test_client.py
@@ -8,18 +8,50 @@ pytestmark = pytest.mark.django_db
 def client():
     return Client()
 
-
-def test_get_categories(client):
+@pytest.fixture(scope='module')
+def categories(client):
     response = client.get("/categories/")
     content = response.json()
-    assert set(["next", "previous", "results"]).issubset(content.keys())
+    return content
 
+
+@pytest.fixture(scope='module')
+def category_products(client):
+    response = client.get("/category/1/")
+    content = response.json()
+    return content
+    
+
+def test_get_categories(categories):
+    # P1: check for pagination
+    assert set(["next", "previous", "results"]).issubset(categories.keys())
+
+    # P2: check contents
     expeted_entry = {"category_id": 1, "category_name": "accessories"}
-    actual_entry = content.get("results")[0]
+    actual_entry = categories.get("results")[0]
     assert actual_entry == expeted_entry
 
 
-def test_get_category_products(client):
-    response = client.get("/category/1/")
-    content = response.json()
-    assert set(["next", "previous", "results"]).issubset(content.keys())
+def test_get_categories_negative(client):
+    r = client.get('/categories/420/')
+    assert r.status_code == 404
+    assert "No Categories matches the given query" in r.json()['detail'] 
+
+
+def test_get_category_products(category_products):
+    # P1: check for pagination
+    assert set(["next", "previous", "results"]).issubset(category_products.keys())
+
+    # P2: check for required keys
+    required_fields = [
+        "product_id",
+        "product_name",
+        "price",
+        "style_name",
+        "color",
+        "preview_image",
+        "description",
+    ]
+    entry = category_products['results'][0]
+    assert set(required_fields).issubset(entry.keys())
+    
