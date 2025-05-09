b7045cb69f37b0021b4c4830f91d5f4cd6cbd355
vanessapigwin <72374043+vanessapigwin@users.noreply.github.com>
2025-05-10 06:04:44 +0800

    add all homepage pics

--- frontend/src/app/home/HomePage.jsx
+++ frontend/src/app/home/HomePage.jsx
@@ -5,16 +5,35 @@ import img2 from "../../assets/JoleenJackalope.jpg";
 import img3 from "../../assets/bayerngirl.jpg";
 import img4 from "../../assets/CatLassTitch.jpg";
 import img5 from "../../assets/Minymints.jpg";
+import { useState } from "react";
+import { useEffect } from "react";
 
 const images = [img1, img2, img3, img4, img5];
 
 export default function Homepage() {
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    const key = setInterval(() => {
+      setCounter((counter) => counter + 1);
+    }, 5000);
+
+    return () => {
+      clearInterval(key);
+    };
+  }, []);
+
+  const imgIdx = counter % 5;
+  const img = images[imgIdx]
+  const author = `/u/${img.split("/").pop().split(".").at(0)}`;
+
   return (
     <div className="mainpage">
       <MainNavbar />
-      <div className="img-slides">
-        <img src={img1}></img>
+      <div className="slide-container">
+        <img className="slide" src={images[imgIdx]}></img>
       </div>
+      <div className="author">{author}</div>
     </div>
   );
 }
