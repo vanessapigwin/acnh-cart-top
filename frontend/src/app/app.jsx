9f4c9c134b20793ee24b6da18571cd040e63bb39
vanessapigwin <72374043+vanessapigwin@users.noreply.github.com>
2025-05-14 19:04:11 +0800

    pass cart item and setter

--- frontend/src/app/app.jsx
+++ frontend/src/app/app.jsx
@@ -2,22 +2,26 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Homepage from "./home/HomePage";
 import StorePage from "./store/StorePage";
 import Cart from "./cart/Cart";
+import { useState } from "react";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Homepage />,
-  },
-  {
-    path: "store",
-    element: <StorePage />,
-  },
-  {
-    path: "cart",
-    element: <Cart />,
-  },
-]);
 
 export default function App() {
+  const [cartItems, setCartItems] = useState([]);
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Homepage />,
+    },
+    {
+      path: "store",
+      element: <StorePage handleAddCart={setCartItems}/>,
+    },
+    {
+      path: "cart",
+      element: <Cart cartItems={cartItems} />,
+    },
+  ]);
+
   return <RouterProvider router={router} />;
 }
--- frontend/src/app/cart/Cart.jsx
+++ frontend/src/app/cart/Cart.jsx
@@ -1,38 +1,17 @@
 import MainNavbar from "../../components/MainNavbar";
 import "./cart.css";
 
-const fakeData = [
-  {
-    "variant_id": 3042,
-    "product_name": "Acorn Knit Cap",
-    "price": 880,
-    "color": "brown",
-    "filename": "https://images.cattoviz.com/Headwear/CapHatAcorn3.png",
-    "quantity": 1
-  },
-  {
-    "variant_id": 3047,
-    "product_name": "Acorn Knit Cap",
-    "price": 880,
-    "color": "beige",
-    "filename": "https://images.cattoviz.com/Headwear/CapHatAcorn5.png",
-    "quantity": 2
-  }
-]
-
-function CartContent() {
-  const data =fakeData
+function CartContent({ cartItems }) {
+  const data = cartItems;
   return (
     <div className="cart-content">
       <h1>My cart</h1>
       <br />
       <div className="cart-content-details">
         <ul className="cart-content-list">
-          {
-            data.map((d) => {
-              return <li key={d.variant_id}>{d.product_name}</li>
-            })
-          }
+          {data.map((d) => {
+            return <li key={d.variant_id}>{d.product_name}</li>;
+          })}
         </ul>
         <div className="cart-summary">
           <h2>Order Summary</h2>
@@ -44,11 +23,11 @@ function CartContent() {
   );
 }
 
-export default function Cart() {
+export default function Cart({ cartItems }) {
   return (
     <div className="cart-page">
       <MainNavbar />
-      <CartContent />
+      <CartContent cartItems={cartItems} />
     </div>
   );
 }
